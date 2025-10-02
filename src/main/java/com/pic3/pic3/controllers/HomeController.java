f4ed7fb0edb0a79240585c636db6e68cf793491e
Vitor Jensen <vitorjensen6@gmail.com>
2025-10-02 09:40:48 -0300

    Dashboard - ajuste de layout

--- src/main/java/com/pic3/pic3/controllers/HomeController.java
+++ src/main/java/com/pic3/pic3/controllers/HomeController.java
@@ -1,13 +1,16 @@
 package com.pic3.pic3.controllers;
 
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
     @GetMapping("/")
-    public String home(){
-        return "pages/index";
+    public String home(Model model){
+        model.addAttribute("title", "Dashboard");
+        return "pages/index"; // sem .html
     }
 }
