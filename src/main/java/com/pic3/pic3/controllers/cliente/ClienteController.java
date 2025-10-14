159925e260592d8b112347ec9781c5971f6d8765
Vitor Jensen <vitorjensen6@gmail.com>
2025-10-14 16:53:55 -0300

    Inicio da tela de Cliente

---
+++ src/main/java/com/pic3/pic3/controllers/cliente/ClienteController.java
@@ -0,0 +1,25 @@
+package com.pic3.pic3.controllers.cliente;
+
+import com.pic3.pic3.service.ClienteService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cliente")
+public class ClienteController {
+
+    private final ClienteService clienteService;
+    public ClienteController(ClienteService clienteService)
+    {
+        this.clienteService = clienteService;
+    }
+
+    @GetMapping
+    public String listarClientes(Model model)
+    {
+        model.addAttribute("clientes", clienteService.findAll());
+        return "cliente/index";
+    }
+}
