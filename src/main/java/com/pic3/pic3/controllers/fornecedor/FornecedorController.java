1c150fe39bbb200ef65c64f79e5ec89cf281379b
Vitor Jensen <vitorjensen6@gmail.com>
2025-11-16 17:08:58 -0300

    Criação das funções CRUD para Fornecedor

--- src/main/java/com/pic3/pic3/controllers/fornecedor/FornecedorController.java
+++ src/main/java/com/pic3/pic3/controllers/fornecedor/FornecedorController.java
@@ -1,10 +1,11 @@
 package com.pic3.pic3.controllers.fornecedor;
 
+import com.pic3.pic3.model.Cliente;
+import com.pic3.pic3.model.Fornecedor;
 import org.springframework.ui.Model;
 import com.pic3.pic3.service.FornecedorService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -24,4 +25,26 @@ public class FornecedorController {
     model.addAttribute("fornecedores", fornecedorService.listarTodos());
     return  "pages/fornecedor";
     }
+
+@PostMapping("/salvar")
+    public String salvarFornecedor(@ModelAttribute Fornecedor fornecedor)
+{
+    fornecedorService.salvar(fornecedor);
+    return "redirect:/fornecedor";
+}
+
+@GetMapping("/deletar/{id}")
+    public String deletarFornecedor(@PathVariable Integer id)
+    {
+    fornecedorService.deletar(id);
+    return "redirect:/fornecedor";
+    }
+
+@PostMapping("/fornecedor/editar")
+        public String editarFornecedor(Fornecedor fornecedor)
+        {
+        fornecedorService.salvar(fornecedor);
+        return "redirect:/fornecedor";
+        }
+
 }
--- src/main/java/com/pic3/pic3/service/FornecedorService.java
+++ src/main/java/com/pic3/pic3/service/FornecedorService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
 @Service
 public class FornecedorService {
     private final FornecedorRepository repo;
@@ -17,6 +18,35 @@ public class FornecedorService {
     public List<Fornecedor> listarTodos()
     {
         return repo.findAll();
+    }
+
+    public Fornecedor salvar(Fornecedor fornecedor)
+    {
+        return repo.save(fornecedor);
+    }
+
+    public Fornecedor atualizar(Integer id, Fornecedor fornecedorAtualizado)
+    {
+        return repo.findById(id)
+                .map(fornecedor -> {
+                    fornecedor.setPessoa(fornecedorAtualizado.getPessoa());
+                    fornecedor.setCnpj(fornecedorAtualizado.getCnpj());
+                    fornecedor.setRazao(fornecedorAtualizado.getRazao());
+                    fornecedor.setFantasia(fornecedorAtualizado.getFantasia());
+                    fornecedor.setEndereco(fornecedorAtualizado.getEndereco());
+                    fornecedor.setNumero(fornecedorAtualizado.getNumero());
+                    fornecedor.setCidade(fornecedorAtualizado.getCidade());
+                    fornecedor.setEstado(fornecedorAtualizado.getEstado());
+                    fornecedor.setTelefone(fornecedorAtualizado.getTelefone());
+                    fornecedor.setDataCadastro(fornecedorAtualizado.getDataCadastro());
+                    return repo.save(fornecedor);
+                })
+                .orElseThrow(() -> new RuntimeException("Cliente não encontrado com ID " + id));
+    }
+
+    public void deletar(Integer id) {
+
+        repo.deleteById(id);
     }
 
 }
